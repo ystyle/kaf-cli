27240c08a130fa715003d89de8744485574d8711
ystyle <lxy5266@live.com>
2020-03-11 11:30:23 +0800

    调整格式

--- main.go
+++ main.go
@@ -216,6 +216,13 @@ func main() {
 }
 
 func AddPart(buff *bytes.Buffer, content string) {
+	if strings.HasSuffix(content, "==") ||
+		strings.HasSuffix(content, "**") ||
+		strings.HasSuffix(content, "--") ||
+		strings.HasSuffix(content, "//") {
+		buff.WriteString(content)
+		return
+	}
 	buff.WriteString(htmlPStart)
 	buff.WriteString(content)
 	buff.WriteString(htmlPEnd)
